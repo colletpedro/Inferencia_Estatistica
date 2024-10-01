65fdb634bd9a8abedf9d966372753624a2095a17
Pedro Collet <pedroocollet@gmail.comm>
2024-10-01 01:02:52 -0300

    testes

--- Inferencia_Estatistica.py
+++ Inferencia_Estatistica.py
@@ -3,12 +3,13 @@ import random
 opcoes = ["Pedra", "Papel", "Tesoura"]
 jogador = random.choice(opcoes)
 computador = random.choice(opcoes)
-
+#quem he quem
 print(f"Jogador: {jogador}")
 print(f"Computador: {computador}")
 
 if jogador == computador:
     print("Empate!")
+    #blablabla
 elif (jogador == "Pedra" and computador == "Tesoura") or \
      (jogador == "Papel" and computador == "Pedra") or \
      (jogador == "Tesoura" and computador == "Papel"):
